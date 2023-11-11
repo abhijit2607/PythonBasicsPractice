89a3bd56013bf08b96ec8144b69543dd15eccde2
Abhijit <paulabhijit2607@gmail.com>
2023-11-11 15:21:28 +0530

    Revert "Added a newTrash.py which finds the factorial of user-defined number (also to practice revert command in git)."
    This reverts commit b3f8b9c56823a8e20613cbb0eed44f3d7091fafa.

--- newTrash.py
+++
@@ -1,9 +0,0 @@
-def factorial(num):
-    if num < 1:
-        return 1
-    return num * factorial(num-1)
-
-#__main__
-
-num1 = int(input("Enter the number to find out its factorial: "))
-print(f"The factorial of {num1} is {factorial(num)}")
