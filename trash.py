e70bf656480afcfe5fc6da7f86254be391ef98b1
abhijit2607 <127297631+abhijit2607@users.noreply.github.com>
2023-11-12 19:26:00 +0530

    Fixed the logicError in the trash.py file

--- trash.py
+++ trash.py
@@ -1,9 +1,11 @@
 def fibonacci(n):
-  if n<0:
+  if n==0:
     return 0
-  return fibonacci(n-1) * fibonacci(n-2)
+  elif n==1:
+    return 1
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
 
-#__main__
-
-num = int(input("Enter the nth term of the fibonacci series: "))
-print(f"The {num}th term of the fibonacci series is: {fibonacci(num)}."))
+# __main__
+num = int(input("Enter the nth term of the Fibonacci series: "))
+print(f"The {num}th term of the Fibonacci series is: {fibonacci(num)}.")
